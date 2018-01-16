ef9f4e0e74bc8fb8c4bf0c07291ab769444c03f7
nvallott <nvallott@hotmail.com>
2018-01-16 14:31:50 +0100

    isochrones with otp server, click and update

--- app.py
+++ app.py
@@ -8,6 +8,43 @@ app.debug = True
 
 # Connect to PostGIS database
 conn = db.connect("dbname='mob_dev' user='nvallott' host='localhost' password=''")
+# 
+# # Creates route to polygon layer
+# @app.route('/geom')
+# def geom(epsg=4326):
+#     return flask.jsonify({ "pixel": do_pixel(epsg), "poly": do_poly(epsg) })
+#
+# @app.route('/pixel')
+# def com(epsg=4326):
+#     return json.dumps(do_pixel(epsg))
+#
+# def do_pixel(epsg=4326):
+#     ###### CHANGE after that sql query to load pixel json
+#
+#     # cur = conn.cursor() # get a query cursor
+#     # # our SQL query:
+#     # sql = """SELECT gmde AS fid, name AS com, ST_AsGeoJson(ST_Transform(geom, %i), 7) AS geom
+#     #          FROM vs_id12""" % epsg
+#     # cur.execute(sql)
+#     # # retrieve the query result
+#     # rows = cur.fetchall()
+#     #
+#     # features = []
+#     # for row in rows:
+#     #     features.append({
+#     #         "type": "Feature",
+#     #         "properties": {
+#     #             "geoid": row[0],
+#     #             "nom": row[1]
+#     #         },
+#     #         "geometry": json.loads(row[2])
+#     #     })
+#     # feature_collection = {
+#     #     "type": "FeatureCollection",
+#     #     "features": features
+#     # }
+#     # return feature_collection
+
 
 @app.route('/')
 def index():
--- static/index.js
+++ static/index.js
@@ -22,18 +22,22 @@ let dataArray = [];
 let dataJson = [];
 // Array of all json datas
 let dataScale = [];
-
 //Color scale of the map
 let colorScaleRange = [];
-
+// get the color from colorbrewer lib
 let colors = colorbrewer.Spectral[11];
+// latitude and longitude from Lausanne station
+var lat = 46.516631
+var lng = 6.629156
+// get data url from the otp server
+let url = "http://localhost:8080/otp/routers/default/isochrone?&fromPlace=" + lat + "," + lng + "&date=2017/12/20&time=07:00:00&mode=TRANSIT,WALK&cutoffSec=1800";
+
 /*****
 Initializing the whole script of the page
 *****/
 APP.main = function(){
     APP.loadData();
     APP.initMap();
-    APP.initSVG();
 };
 
 /*****
@@ -41,22 +45,37 @@ Initializing map - leaflet with cartodb basemap
 *****/
 APP.initMap = function(){
     // Initiaize the map - definig parameters and adding cartodb basemap
-    map = new L.map("map", {center: [46.51522, 6.62981], zoom: 9.5, minZoom: 7, maxZoom: 15, maxBounds: ([[45.8, 5.7],[47.5, 7.9]])});
+    map = new L.map("map", {center: [46.51522, 6.62981], zoom: 9.5, minZoom: 6, maxZoom: 15, maxBounds: ([[45.8, 5.7],[47.5, 7.9]])});
     let cartodb = L.tileLayer('https://cartodb-basemaps-{s}.global.ssl.fastly.net/light_all/{z}/{x}/{y}.png', {
         attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>, &copy;<a href="https://carto.com/attribution">CARTO</a>'
     });
 
     // Add the cartodb layer to the map
     cartodb.addTo(map);
-    };
 
-APP.initSVG = function(){
+    // on click change the datas
+    map.on('click', function(e){
+      var coord = e.latlng;
+      lat = coord.lat;
+      lng = coord.lng;
+      url = "http://localhost:8080/otp/routers/default/isochrone?&fromPlace=" + lat + "," + lng + "&date=2017/12/20&time=07:00:00&mode=CAR&cutoffSec=1800";
+      console.log(url);
+      APP.removeSVG();
+      APP.loadData(url);
+    });
+};
 
+APP.removeSVG = function(){
+  d3.selectAll("path")
+    .remove();
+};
+
+APP.initSVG = function(dataJson){
+    console.log(dataJson, "initSVG");
     var dataOverlay = L.d3SvgOverlay(function(sel, proj) {
-
       var upd = sel.selectAll('path').data(dataJson);
           console.log(dataScale);
-          var color = d3.scale.threshold()
+      var color = d3.scale.threshold()
                         .domain(dataScale.reverse())
                         .range(colors);
           upd.enter()
@@ -86,25 +105,26 @@ APP.initSVG = function(){
     };
 
   // button to switch data
-  L.control.layers({"Data": dataOverlay}).addTo(map);
-
-    // d3.json("iso5.json", function(data) {
-    d3.json("static/iso5.json", function(data) {
+  // L.control.layers({"Data": dataOverlay}).addTo(map);
+    d3.json(dataJson, function(data) {
+    // d3.json("static/iso5.json", function(data) {
       console.log(data);
       dataOverlay.addTo(map) });
 };
 
 APP.loadData = function(){
-    // d3.json("iso5.json", function(data) {
-    d3.json("static/iso5.json", function(data) {
-      console.log(data);
+    d3.json(url, function(data) {
+      console.log(data, "load");
+    // d3.json("static/iso5.json", function(data) {
       APP.jsonToArray(data);
     });
 }
 
+
 // Storing the data into an array
 APP.jsonToArray = function(data){
   dataJson = data.features;
+  console.log(dataJson, "toArray");
   for (let i=0; i < data.features.length; i++){
     dataScale.push(
       data.features[i].properties.time
@@ -119,4 +139,5 @@ APP.jsonToArray = function(data){
       time : data.features[i].properties.time
     });
   }
+  APP.initSVG(dataJson);
 };
