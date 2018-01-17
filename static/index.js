abd2c9131db7453ef38eb47eebf4ab1c883bcf8d
nvallott <nvallott@hotmail.com>
2018-01-17 12:42:46 +0100

    time selector

--- static/index.js
+++ static/index.js
@@ -20,12 +20,16 @@ let color = [];
 let dataArray = [];
 // Array of all json datas
 let dataJson = [];
-// Array of all json datas
+// Array of all time datas
 let dataScale = [];
+// Array of all json datas
+let dataSort = [];
 //Color scale of the map
 let colorScaleRange = [];
 // get the color from colorbrewer lib
-let colors = colorbrewer.GnBu[4];
+let colors;
+let min;
+let max;
 // latitude and longitude from Lausanne station
 let lat = 46.516631;
 let lng = 6.629156;
@@ -33,11 +37,12 @@ let lng = 6.629156;
 let cf = "&cutoffSec="
 let cf1 = cf+600;
 let cf2 = cf+1200;
-let cf3 = cf+1800;
+let cf3 = cf+3000;
 var mode = "TRANSIT,WALK";
+var time = "07:30";
 let baseUrl = "http://localhost:8080/otp/routers/default/isochrone?&fromPlace="
 // get data url from the otp server
-let url = baseUrl + lat + "," + lng + "&date=2017/12/20&time=07:00:00&mode=" + mode + cf1 + cf2 + cf3;
+let url = baseUrl + lat + "," + lng + "&date=2017/12/20&time=" + time + "&mode=" + mode + cf1 + cf2 + cf3;
 
 /*****
 Initializing the whole script of the page
@@ -81,17 +86,20 @@ APP.initMap = function(){
       var coord = e.latlng;
       lat = coord.lat;
       lng = coord.lng;
-      url = baseUrl + lat + "," + lng + "&date=2017/12/20&time=07:00:00&mode=" + mode + cf1 + cf2 + cf3;
+      url = baseUrl + lat + "," + lng + "&date=2017/12/20&time=" + time + "&mode=" + mode + cf1 + cf2 + cf3;
       console.log(url);
-      APP.removeSVG();
       APP.loadData(url);
     });
     $('.transport-mode select').on('change', function(){
       mode = $('.transport-mode select').val();
-      console.log(mode);
-      url = baseUrl + lat + "," + lng + "&date=2017/12/20&time=07:00:00&mode=" + mode + cf1 + cf2 + cf3;
+      url = baseUrl + lat + "," + lng + "&date=2017/12/20&time=" + time + "&mode=" + mode + cf1 + cf2 + cf3;
       console.log(url);
-      APP.removeSVG();
+      APP.loadData(url);
+    });
+    $('.transport-time input').on('change', function(){
+      time = $('.transport-time input').val();
+      url = baseUrl + lat + "," + lng + "&date=2017/12/20&time=" + time + "&mode=" + mode + cf1 + cf2 + cf3;
+      console.log(url);
       APP.loadData(url);
     });
 
@@ -102,50 +110,37 @@ APP.removeSVG = function(){
 };
 
 APP.initSVG = function(dataJson){
+  APP.removeSVG();
+  dataSort = dataScale.sort(function(a, b) {
+    return a - b;
+  });
+  console.log(dataSort);
+  var color = d3.scale.threshold()
+                      // .domain([600, 1200, 1800, 2400, 3000, 3600, 4200, 4800, 5400, 6000])
+                      .domain(dataSort)
+                      .range(colors);
+
     var dataOverlay = L.d3SvgOverlay(function(sel, proj) {
       var upd = sel.selectAll('path').data(dataJson);
-          console.log(dataScale);
-          console.log(colors);
-      var color = d3.scale.threshold()
-                          // .domain([600, 1200, 1800, 2400, 3000, 3600, 4200, 4800, 5400, 6000])
-                          .domain(dataScale.reverse())
-                          .range(colors);
+
           upd.enter()
              .append('path')
              .attr('d', proj.pathFromGeojson)
              .attr('fill-opacity', '0.2')
              .attr('fill', function(d){ return color(d.properties.time)})
-             .on('mouseover', mover)
-             .on('mouseout', mout);
           upd.attr('stroke-width', 0.1 / proj.scale); // for updating the stroke when zooming
       });
 
-    //Mouseover function NOT WORKING
-    function mover(d) {
-      var el = d3.select(this)
-    		.transition()
-    		.duration(10)
-    		.style("fill-opacity", 0.3);
-    }
-
-    //Mouseout function NOT WORKING
-    function mout(d) {
-    	var el = d3.select(this)
-    	   .transition()
-    	   .duration(1000)
-    	   .style("fill-opacity", 1);
-    };
-
-  // button to switch data
-  // L.control.layers({"Data": dataOverlay}).addTo(map);
     d3.json(dataJson, function(data) {
     // d3.json("static/iso5.json", function(data) {
       dataOverlay.addTo(map) });
 };
 
 APP.loadData = function(){
-    d3.json(url, function(data) {
-      // console.log(data, "load");
+    d3.json(url, function(error, data) {
+      if(error) {
+        console.log(error);
+      }
     // d3.json("static/iso5.json", function(data) {
       APP.jsonToArray(data);
     });
@@ -154,6 +149,8 @@ APP.loadData = function(){
 // Storing the data into an array
 APP.jsonToArray = function(data){
   dataJson = data.features;
+  colors = colorbrewer.GnBu[data.features.length+1]; // need n+1 of the domain
+  console.log(colors);
   // empty array to push new datas
   dataScale = [];
   for (let i=0; i < data.features.length; i++){
@@ -170,5 +167,5 @@ APP.jsonToArray = function(data){
       time : data.features[i].properties.time
     });
   }
-  APP.initSVG(dataJson, dataScale);
+  APP.initSVG(dataJson);
 };
